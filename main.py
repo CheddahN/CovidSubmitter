51aa3bb4a89112fc44077c985b374bee1b2e04c5
CheddahN <cheddahn@gmail.com>
2021-11-24 19:23:28 -0500

    added data.json support

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import json
+
 import requests
 import datetime
 import schedule
 import time
 from playwright.sync_api import sync_playwright
 
-url = "https://student-covid-screening.dsbn.org/api/vault/status/619e3b591ba66e0026463242"
+file = json.load(open('data.json'))
+url = 'https://student-covid-screening.dsbn.org/api/vault/status/619e3b591ba66e0026463242'
 headers = {
-    "authorization": "",
-    "user-agent": ""
+    'authorization': "",
+    'user-agent': ""
 }
-uids = [354867442, 337352314]
 
 
 def getval(request):
-    if request.header_value("authorization") is not None:
-        headers["authorization"] = request.header_value("authorization")
+    if request.header_value('authorization') is not None:
+        headers['authorization'] = request.header_value('authorization')
         print(headers)
 
 
@@ -24,9 +26,9 @@ def job():
         page = browser.new_page()
         page.goto('https://login.microsoftonline.com/?whr=dsbn.org')
         page.wait_for_load_state()
-        page.fill('[placeholder="email@dsbn.org | loginid@students.dsbn.org"]', "bouffarn@students.dsbn.org")
+        page.fill('[placeholder="email@dsbn.org | loginid@students.dsbn.org"]', file['login']['email'])
         page.click('text="Next"')
-        page.fill('[placeholder="Password"]', "cheeseandcrackersarereallygood555556")
+        page.fill('[placeholder="Password"]', file['login']['password'])
         page.click('text="Sign in"')
 
         page.on('request', lambda request: getval(request))
@@ -34,17 +36,18 @@ def job():
         page.locator('text="Go to school"').wait_for()
         browser.close()
 
-    for user in uids:
+    for user in file['users']:
         data = {
-            "uid": user,
-            "date": datetime.date.today(),
-            "status": "passed"
+            'uid': user,
+            'date': datetime.date.today(),
+            'status': 'passed'
         }
         response = requests.put(url=url, headers=headers, data=data)
-        print(response)
+        print(str(response.status_code) + str(response.json()) + ' ' + file['users'][user]['FirstName'] + ' ' + file['users'][user]['LastName'])
 
 
-schedule.every().day.at("07:00").do(job)
+schedule.every().day.at('06:20').do(job)
+job()
 while True:
     schedule.run_pending()
     time.sleep(1)
