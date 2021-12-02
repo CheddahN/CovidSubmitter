551c3f2c54c6de5b45afba49524501b2241e732e
CheddahN <CheddahN@gmail.com>
2021-12-02 15:19:32 -0500

    initial web version

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import json
-
-import requests
-import datetime
 import schedule
 import time
 from playwright.sync_api import sync_playwright
@@ -14,41 +11,22 @@ headers = {
 }
 
 
-def getval(request):
-    if request.header_value('authorization') is not None:
-        headers['authorization'] = request.header_value('authorization')
-        print(headers)
-
-
 def job():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto('https://login.microsoftonline.com/?whr=dsbn.org')
-        page.wait_for_load_state()
-        page.fill('[placeholder="email@dsbn.org | loginid@students.dsbn.org"]', file['login']['email'])
-        page.click('text="Next"')
-        page.fill('[placeholder="Password"]', file['login']['password'])
-        page.click('text="Sign in"')
-
-        page.on('request', lambda request: getval(request))
-        page.goto('https://student-covid-screening.dsbn.org/pass')
-        page.locator('text="Go to school"').wait_for()
-        browser.close()
-
     for user in file['users']:
-        data = {
-            'uid': user,
-            'date': datetime.date.today(),
-            'status': 'passed'
-        }
-        response = requests.put(url=url, headers=headers, data=data)
-        if response.status_code == 200:
-            print(str(response.status_code) + str(response.json()) + ' ' + file['users'][user]['FirstName'] + ' ' +
-                  file['users'][user]['LastName'])
-        else:
-            print(str(response.status_code) + ' ' + file['users'][user]['FirstName'] + ' ' +
-                  file['users'][user]['LastName'])
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
+            page.goto('https://login.microsoftonline.com/?whr=dsbn.org')
+            page.wait_for_load_state()
+            page.fill('[placeholder="email@dsbn.org | loginid@students.dsbn.org"]', file['users'][user]['email'])
+            page.click('text="Next"')
+            page.fill('[placeholder="Password"]', file['users'][user]['password'])
+            page.click('text="Sign in"')
+
+            page.goto('https://student-covid-screening.dsbn.org/pass')
+            page.locator('text="Go to school"').wait_for()
+            browser.close()
+            print("seccess " + user)
 
 
 schedule.every().day.at('06:20').do(job)
